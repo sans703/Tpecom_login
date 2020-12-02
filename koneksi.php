c4cb16c3fa8dc8b49e10eb3615a3dad3e5dfe25f
sans703 <egamhndr@gmail.com>
2020-12-02 13:09:54 +0700

    login project

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$koneksi = mysqli_connect("localhost","root","","tp_login");
+
+// Check connection
+if (mysqli_connect_errno()){
+ echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+
+?>
